62cd622a9959a761c9a83ec74d68e23034ff6a5f
Victor Elias <victorgelias@gmail.com>
2021-04-01 13:04:21 -0300

    Create global tx rate limiter

--- authorizer/authorizer.go
+++ authorizer/authorizer.go
@@ -6,7 +6,8 @@ import (
 )
 
 type Authorizer struct {
-	accountState *model.Account
+	accountState   *model.Account
+	globalLimiter  *RateLimiter
 }
 
 func (a *Authorizer) CreateAccount(account *model.Account) (model.Account, error) {
@@ -32,6 +33,12 @@ func (a *Authorizer) PerformTransaction(transaction *model.Transaction) (model.A
 	}
 	if account.AvailableLimit < transaction.Amount {
 		err := violation.NewError(violation.InsufficientLimit, "Transaction amount is higher than available limit")
+		return model.Account{}, err
+	}
+	if ok, err := a.globalLimiter.Take(transaction.Time); err != nil {
+		return model.Account{}, err
+	} else if !ok {
+		err := violation.NewError(violation.HighFrequencySmallInterval, "Too many transactions in a small interval")
 		return model.Account{}, err
 	}
 	account.AvailableLimit -= transaction.Amount
