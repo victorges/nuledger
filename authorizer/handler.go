123133aba32ff1d5cd6dbb9e39184290631254c8
Victor Elias <victorgelias@gmail.com>
2021-03-31 21:44:52 -0300

    Refactor handler a bit for the future

--- authorizer/handler.go
+++ authorizer/handler.go
@@ -2,6 +2,7 @@ package authorizer
 
 import (
 	"errors"
+	"fmt"
 	"nuledger/iop"
 	"nuledger/model"
 	"nuledger/model/violation"
@@ -15,26 +16,47 @@ func NewHandler() iop.DataHandler {
 	return &Handler{Authorizer{}}
 }
 
+type operationType int
+
+const (
+	operationTypeUnknown operationType = iota
+	operationTypeCreateAccount
+	operationTypePerformTransaction
+)
+
 func (h *Handler) Handle(op model.OperationInput) (model.StateOutput, error) {
-	if op.Account != nil && op.Transaction != nil {
-		return model.StateOutput{}, errors.New("Must have only 1 of account or transaction fields set")
-	} else if op.Account != nil {
-		account, err := h.CreateAccount(op.Account)
-		var verr *violation.Error
-		if err != nil && !errors.As(err, &verr) {
-			return model.StateOutput{}, err
-		}
-		return newStateOutput(account, verr), nil
-	} else if op.Transaction != nil {
-		account, err := h.PerformTransaction(op.Transaction)
-		var verr *violation.Error
-		if err != nil && !errors.As(err, &verr) {
-			return model.StateOutput{}, err
-		}
-		return newStateOutput(account, verr), nil
-	} else {
-		return model.StateOutput{}, errors.New("Must have either account or transaction fields set")
+	opType, err := getOperationType(op)
+	if err != nil {
+		return model.StateOutput{}, fmt.Errorf("Bad operation object: %w", err)
 	}
+
+	var account model.Account
+	switch opType {
+	case operationTypeCreateAccount:
+		account, err = h.CreateAccount(op.Account)
+	case operationTypePerformTransaction:
+		account, err = h.PerformTransaction(op.Transaction)
+	default:
+		return model.StateOutput{}, errors.New("Internal error: Unknown operation type")
+	}
+
+	var verr *violation.Error
+	if err != nil && !errors.As(err, &verr) {
+		return model.StateOutput{}, err
+	}
+	return newStateOutput(account, verr), nil
+}
+
+func getOperationType(op model.OperationInput) (operationType, error) {
+	hasAccount := op.Account != nil
+	hasTransaction := op.Transaction != nil
+	if hasAccount == hasTransaction {
+		return operationTypeUnknown, errors.New(`Must have exactly 1 of "account" or "transaction" fields set`)
+	}
+	if hasAccount {
+		return operationTypeCreateAccount, nil
+	}
+	return operationTypePerformTransaction, nil
 }
 
 func newStateOutput(account model.Account, err *violation.Error) model.StateOutput {
