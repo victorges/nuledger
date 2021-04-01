90c2e42dac9b0eb76fdb6ea43dfe8c4fc93ccacf
Victor Elias <victorgelias@gmail.com>
2021-04-01 12:42:03 -0300

    Create real account creation logic

--- authorizer/authorizer.go
+++ authorizer/authorizer.go
@@ -2,12 +2,22 @@ package authorizer
 
 import (
 	"nuledger/model"
+	"nuledger/model/violation"
 )
 
-type Authorizer struct{}
+type Authorizer struct {
+	accountState *model.Account
+}
 
 func (a *Authorizer) CreateAccount(account *model.Account) (model.Account, error) {
-	return *account, nil
+	if a.accountState != nil {
+		err := violation.NewError(violation.AccountAlreadyInitialized, "Account has already been initialized")
+		return *a.accountState, err
+	}
+
+	a.accountState = &model.Account{}
+	*a.accountState = *account
+	return *a.accountState, nil
 }
 
 func (a *Authorizer) PerformTransaction(transaction *model.Transaction) (model.Account, error) {
