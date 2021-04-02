83fda4dde99e8502b48a2986ef2c9767659b6e45
Victor Elias <victorgelias@gmail.com>
2021-04-02 18:36:21 -0300

    Improve docs

--- authorizer/rule/authorizer.go
+++ authorizer/rule/authorizer.go
@@ -4,18 +4,20 @@ import (
 	"nuledger/model"
 )
 
-// CommitFunc is a function that can be returned by rule.Authorizer's to be
+// CommitFunc is a function that can be returned by an Authorizer, for it to be
 // called as a confirmation that the transaction was executed. It can be used
-// to update the internal state of the authorizer so as to guarantee future
+// to update the internal state of the Authorizer so as to guarantee future
 // authorizations are performed consistently, considering only the actually
-// executed transactions.
+// executed transactions, not the attempted ones.
 type CommitFunc func()
 
-// Authorizer is the interface for any rule to be enforced when authorizing a
-// transaction on a given account. If the rule is broken, an error should be
-// returned by the Authorize function, which can be of type violation.Error to
-// return the corresponding error code to the user (instead of a fatal error).
-// It can also return a CommitFunc which should be called by the transaction
+// An Authorizer enforces a rule when performing a transaction on an account.
+//
+// Authorize should return an error if the rule is broken, which can be of type
+// violation.Error to return the corresponding error code to the user (instead
+// of a fatal error).
+//
+// It can also return a CommitFunc which will be called by the transaction
 // execution agent in case the transaction is actually executed in the end
 // (i.e. if no other rules blocked that transaction).
 type Authorizer interface {
