07bcfd74884f74847303708766996610dabcfa40
Victor Elias <victorgelias@gmail.com>
2021-04-04 14:18:13 -0300

    Create separate doc.go file in authozier
    Only package that has some longer doc, the other ones
    are so short it seems like an overkill to separate.

--- authorizer/ledger.go
+++ authorizer/ledger.go
@@ -1,9 +1,3 @@
-// Package authorizer contains the higher-level components of the authorizer
-// logic.
-//
-// It's main components are a Ledger which handles the core business
-// logic of creating accounts and performing transactions and a Handler which
-// translates between lower-level JSON messages and the Ledger interface.
 package authorizer
 
 import (
