2f22e0b837b91c80223f648828687a68a0c6f3d5
Victor Elias <victorgelias@gmail.com>
2021-04-04 15:13:11 -0300

    Fix tests and mocks as a whole
     - Execute all tests in project with go test ./...
     - Fix output path of mocks so they're not all in the same package
     - Fix package name of mocks

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ run:
 	go run main.go
 
 test:
-	go test
+	go test ./...
 
 generate:
 	go generate ./...
--- gen_mocks.sh
+++ gen_mocks.sh
@@ -5,10 +5,10 @@ mock_target="$1"
 project_dir=$(dirname $(realpath $0))
 
 abs_cwd=$(pwd)
-relative_cwd=".${abs_cwd#"$project_dir"}"
+relative_cwd="${abs_cwd#"$project_dir/"}"
 
-echo "Mocking interfaces in $relative_cwd/$mock_target to ./mocks/$mock_target"
+echo "Mocking interfaces in ./$relative_cwd/$mock_target to ./mocks/$relative_cwd/$mock_target"
 
 mock_src="./$mock_target"
-mock_dest="$project_dir/mocks/$mock_target"
-go run github.com/golang/mock/mockgen -package mocks -source="$mock_src" -destination="$mock_dest"
+mock_dest="$project_dir/mocks/$relative_cwd/$mock_target"
+go run github.com/golang/mock/mockgen -source="$mock_src" -destination="$mock_dest"
