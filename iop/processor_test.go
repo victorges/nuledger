54e9dad89915c6484adabff082a88b647c7c5e76
Victor Elias <victorgelias@gmail.com>
2021-04-04 17:03:12 -0300

    Implement original version of processor tests

--- iop/processor_test.go
+++ iop/processor_test.go
@@ -1,13 +1,20 @@
 package iop_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"nuledger/iop"
 	mock_iop "nuledger/mocks/iop"
+	"nuledger/model"
+	"nuledger/model/violation"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+var startTime = time.Date(2021, time.March, 31, 14, 57, 55, 0, time.Local)
 
 func TestIOProcessor(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -16,26 +23,37 @@ func TestIOProcessor(t *testing.T) {
 	Convey("Input/Output Processor", t, func() {
 		handler := mock_iop.NewMockDataHandler(ctrl)
 
-		handler.EXPECT().
-			Handle(gomock.AssignableToTypeOf(&iop.OperationInput{})).
-			Return(iop.StateOutput{}, nil).
-			AnyTimes()
+		in, out := bytes.NewBuffer(nil), bytes.NewBuffer(nil)
+		processor := iop.NewProcessor(in, out, handler)
 
-		processor := iop.NewProcessor(nil, nil, handler)
-		_ = processor
-
-		x := 0
-		Convey("When the integer is incremented", func() {
-			x++
-
-			Convey("The value should be greater by one", func() {
-				So(x, ShouldEqual, 1)
-			})
-
-			x++
-			Convey("The value should be greater by two", func() {
-				So(x, ShouldEqual, 2)
-			})
+		Convey("Does no-op on no input", func() {
+			err := processor.Process()
+			So(err, ShouldBeNil)
+			So(out.Len(), ShouldEqual, 0)
 		})
+
+		Convey("Forwards read and received objects with no change", func() {
+			input := iop.OperationInput{
+				Account:     &model.Account{true, 1337},
+				Transaction: &model.Transaction{"sketchy", 420, startTime},
+			}
+			output := iop.StateOutput{
+				Account:    &model.Account{false, 7331},
+				Violations: []violation.Code{"not-even-a-violation"},
+			}
+
+			handler.EXPECT().
+				Handle(gomock.Eq(input)).
+				Return(output, nil)
+
+			err := json.NewEncoder(in).Encode(input)
+			So(err, ShouldBeNil)
+			err = processor.Process()
+
+			So(err, ShouldBeNil)
+			So(out.Len(), ShouldEqual, 95)
+			// TODO: check output object
+		})
+
 	})
 }
