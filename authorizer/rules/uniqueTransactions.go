b5b49433237d47e55f2b443aed6b2ca8812b3a57
Victor Elias <victorgelias@gmail.com>
2021-04-17 19:31:18 -0300

    Make unique transaction analyzer more generic
    Will be the base for the limited frequency one.

--- authorizer/rules/uniqueTransactions.go
+++ authorizer/rules/uniqueTransactions.go
@@ -12,25 +12,39 @@ import (
 // transactions are allowed to go through in the account. For 2 transactions to
 // be considered double, they must have the exact same merchant and amount, and
 // have timestamps within the configured interval of each other.
-type UniqueTransactions struct {
-	doubleTxInterval time.Duration
-	limiters         map[doubleTransactionKey]*util.RateLimiter
+type FrequencyAnalyzer struct {
+	baseLimiter *util.RateLimiter
+	limiters    map[interface{}]*util.RateLimiter
+	keyMapper   func(*model.Transaction) interface{}
+	violation   violation.Error
 }
 
 // NewUniqueTransactions returns a UniqueTransactions authorizer with the given
 // configuration. The `interval` is the minimum amount of time between two
 // transactions with everything else equal for them not to glbe considered double.
-func NewUniqueTransactions(interval time.Duration) *UniqueTransactions {
-	return &UniqueTransactions{interval, map[doubleTransactionKey]*util.RateLimiter{}}
+func NewUniqueTransactions(interval time.Duration) rule.Authorizer {
+	keyMapper := func(tx *model.Transaction) interface{} {
+		return doubleTransactionKey{
+			AccountID: tx.AccountID,
+			Merchant:  tx.Merchant,
+			Amount:    tx.Amount,
+		}
+	}
+	return &FrequencyAnalyzer{
+		baseLimiter: &util.RateLimiter{MaxEvents: 1, Interval: interval},
+		limiters:    map[interface{}]*util.RateLimiter{},
+		keyMapper:   keyMapper,
+		violation:   violation.ErrorDoubleTransaction,
+	}
 }
 
 // Authorize checks if the given transaction is a double, considering the
 // configured interval, and if so the transaction is not authorized and a
 // double-transaction violation error is returned.
-func (d *UniqueTransactions) Authorize(_ model.Account, transaction model.Transaction) (rule.CommitFunc, error) {
+func (d *FrequencyAnalyzer) Authorize(_ model.Account, transaction model.Transaction) (rule.CommitFunc, error) {
 	limiter := d.getLimiter(&transaction)
 	if !limiter.Allows(transaction.Time) {
-		return nil, violation.ErrorDoubleTransaction
+		return nil, d.violation
 	}
 	commit := func() { limiter.Take(transaction.Time) }
 	return commit, nil
@@ -44,15 +58,12 @@ type doubleTransactionKey struct {
 
 // getLimiter tries to get the existing rate limiter for a given transaction and
 // creates a new one if there is none yet.
-func (d *UniqueTransactions) getLimiter(transaction *model.Transaction) *util.RateLimiter {
-	key := doubleTransactionKey{
-		AccountID: transaction.AccountID,
-		Merchant:  transaction.Merchant,
-		Amount:    transaction.Amount,
-	}
+func (d *FrequencyAnalyzer) getLimiter(transaction *model.Transaction) *util.RateLimiter {
+	key := d.keyMapper(transaction)
 	limiter := d.limiters[key]
 	if limiter == nil {
-		limiter = &util.RateLimiter{MaxEvents: 1, Interval: d.doubleTxInterval}
+		copy := *d.baseLimiter
+		limiter = &copy
 		d.limiters[key] = limiter
 	}
 	return limiter
